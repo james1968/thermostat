f0972e16998a7d25d5652f8e9631432d70b914ae
James <bakerje@mac.com>
2016-10-25 14:59:26 +0100

    Added getTemperature function and test for increasing temperature

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -3,11 +3,18 @@
 describe('Thermostat',function() {
 
   var thermostat;
+
   beforeEach(function() {
     thermostat = new Thermostat();
   });
 
   it('starts at 20 degrees',function() {
-    expect(thermostat.temperature()).toEqual(20);
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
+
+  it('increases in temperature with the up button', function(){
+    thermostat.increaseTemperature();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
+
 });
