fb9ab36561552bc40571252078cef00f9d9c6d4b
James <bakerje@mac.com>
2016-10-25 19:53:46 +0100

    Changes for max temp

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -45,4 +45,20 @@ describe('Thermostat',function() {
     expect(thermostat.isPowerSavingModeOn()).toBe(true)
   });
 
+  describe('when power saving mode is on', function() {
+    it('has a maximum temperature of 25 degrees', function() {
+      for (var i = 0; i < 6; i++) {
+        thermostat.increaseTemperature();
+      }
+      expect(thermostat.getCurrentTemperature()).toEqual(25);
+    });
+  });
+  describe('when power saving mode is off', function() {
+    it('has a maximum temperature of 32 degrees', function() {
+      for (var i = 0; i < 13; i++) {
+        thermostat.increaseTemperature();
+      }
+      expect(thermostat.getCurrentTemperature()).toEqual(32);
+    });
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,9 +1,11 @@
 'use strict'
 
 function Thermostat() {
-  this.MINIMUM_TEMPERATURE = 10;
+  this.MINIMUM_TEMPERATURE = 10
   this.temperature = 20
   this.powerSavingMode = true
+  this.MAX_LIMIT_PSM_ON = 25
+  this.MAX_LIMIT_PSM_OFF = 32
 }
 
 Thermostat.prototype.getCurrentTemperature = function() {
@@ -11,8 +13,11 @@ Thermostat.prototype.getCurrentTemperature = function() {
 }
 
 Thermostat.prototype.increaseTemperature = function() {
+  if (this.isMaximumTemperature()) {
+    return;
+  }
   this.temperature += 1;
-};
+}
 
 Thermostat.prototype.decreaseTemperature = function() {
   if (this.isMinimumTemperature()) {
@@ -32,3 +37,15 @@ Thermostat.prototype.isPowerSavingModeOn = function() {
 Thermostat.prototype.switchPowerSavingModeOff = function() {
   this.powerSavingMode = false
 }
+
+Thermostat.prototype.switchPowerSavingModeOn = function() {
+  this.powerSavingMode = true
+}
+
+Thermostat.prototype.isMaximumTemperature = function() {
+  if (this.isPowerSavingModeOn() === false) {
+    return this.temperature === this.MAX_LIMIT_PSM_OFF;
+  } else {
+    return this.temperature === this.MAX_LIMIT_PSM_OFF;
+  };
+};
