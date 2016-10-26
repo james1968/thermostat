3d508a963d6c233304450957a981f1facddbf0fb
souljuse <oppo.irene@gmail.com>
2016-10-26 16:39:53 +0200

    powersaving mode on and off

--- js/interface.js
+++ js/interface.js
@@ -12,4 +12,20 @@ $(document).ready(function() {
     thermostat.decreaseTemperature();
     updateTemperature()
   })
+
+  $('#temperature-reset').click(function(){
+    thermostat.resetTemperature();
+    updateTemperature()
+  })
+
+  $('#powersaving-on').click(function(){
+    thermostat.switchPowerSavingModeOn();
+    $('#power-saving-status').text('on')
+  })
+
+  $('#powersaving-off').click(function(){
+    thermostat.switchPowerSavingModeOff();
+    $('#power-saving-status').text('off')
+  })
+
 })
