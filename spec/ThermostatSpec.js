5618cfddae18554f63ef6a428cb2beeff5bd6e80
Amaal Ali <amq.ali@gmail.com>
2016-10-26 19:45:02 +0100

    1) If power saving mode is on, the maximum temperature is 25 degrees 2) If power saving mode is off, the maximum temperature is 32 degrees 3) Guard for edge case with temp

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -46,9 +46,18 @@ describe('Thermostat', function() {
             expect(thermostat._powerSaving).toEqual(false);
         });
 
+        it('if current temp is greater than power saving max temp, thermostat lowers to power saving max temp when power saving switched on', function() {
+            var i;
+            for (i = 1; i < (thermostat._POWER_SAVING_ON_MAX_TEMP - thermostat._DEFAULT_TEMPERATURE + 2) ; i++) {
+                thermostat.up();
+            }
+            thermostat.powerSavingOn();
+            expect(thermostat.temperature()).toEqual(thermostat._POWER_SAVING_ON_MAX_TEMP);
+        });
+
         it('maximum temperature is 32 degrees at power saving mode off', function() {
             var i;
-            for (i = 1; i < 12; i++) {
+            for (i = 1; i < 13; i++) {
                 thermostat.up();
             }
             expect(function() { thermostat.up(); }).toThrowError('Power saving off - maximum temperature reached: 32 degrees')
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -4,6 +4,8 @@ function Thermostat() {
     this._DEFAULT_TEMPERATURE = 20;
     this._current_temperature = this._DEFAULT_TEMPERATURE;
     this._powerSaving = true;
+    this._POWER_SAVING_ON_MAX_TEMP = 25;
+    this._POWER_SAVING_OFF_MAX_TEMP = 32;
 };
 
 Thermostat.prototype.temperature = function() {
@@ -11,6 +13,11 @@ Thermostat.prototype.temperature = function() {
 };
 
 Thermostat.prototype.up = function() {
+    if (this._powerSaving === true && this._current_temperature === this._POWER_SAVING_ON_MAX_TEMP) {
+        throw new Error('Power saving on - maximum temperature reached: 25 degrees');
+    } else if (this._powerSaving === false && this._current_temperature === this._POWER_SAVING_OFF_MAX_TEMP) {
+        throw new Error('Power saving off - maximum temperature reached: 32 degrees');
+    }
     return this._current_temperature = this._current_temperature + 1;
 };
 
@@ -26,6 +33,9 @@ Thermostat.prototype.powerSavingOff = function() {
 };
 
 Thermostat.prototype.powerSavingOn = function() {
+    if (this._current_temperature >  this._POWER_SAVING_ON_MAX_TEMP) {
+        this._current_temperature = this._POWER_SAVING_ON_MAX_TEMP;
+    }
     this._powerSaving = true;
 };
 
