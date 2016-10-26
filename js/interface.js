7dbad8681019cca3c9e3ea2ee1821099a34757a2
James <bakerje@mac.com>
2016-10-26 18:26:56 +0100

    Updated for current temp and selector for cities

--- js/interface.js
+++ js/interface.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+    $('#current-temperature').text(data.main.temp);
+  })
+  $('#current-city').change(function() {
+    var city = $('#current-city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+      $('#current-temperature').text(data.main.temp)
+    })
+  })
   var thermostat = new Thermostat();
   function updateTemperature() {
-    $('#temperature').text(thermostat.temperature)
+    $('#temperature').text(thermostat.temperature);
+    $('#temperature').attr('class', thermostat.energyUsage());
   }
   updateTemperature()
   $('#temperature-up').on('click', function() {
