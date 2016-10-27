2ceea6a882b6fa230ed7b8b88dfbd627152a4f05
Christopher Sanchez <sanchez.christopherm@gmail.com>
2016-10-27 14:32:57 +0200

    1) Created interface.js for the Thermostat.js and index.html

--- js/interface.js
+++ js/interface.js
@@ -1,37 +1,38 @@
 $(document).ready(function() {
-  var thermostat = new Thermostat();
-  $('#temperature').text(thermostat.temperature());
+    var thermostat = new Thermostat();
+    $('#temperature').text(thermostat.temperature());
 
-function updateTemperature() {
-  $('#temperature').text(thermostat.temperature());
-}
+    function updateTemperature() {
+        $('#temperature').text(thermostat.temperature());
+        $('#temperature').attr('class', thermostat.energyUse());
+    }
 
-$('#temp-up').click(function() {
-  thermostat.up();
-  updateTemperature();
-});
+    $('#temp-up').click(function() {
+        thermostat.up();
+        updateTemperature();
+    });
 
 
-$('#temp-down').click(function() {
-  thermostat.down();
-  updateTemperature();
-});
+    $('#temp-down').click(function() {
+        thermostat.down();
+        updateTemperature();
+    });
 
-$('#psm-on').click(function() {
-  thermostat.powerSavingOn();
-  $('#power-saving-status').text('ON')
-  updateTemperature();
-});
+    $('#psm-on').click(function() {
+        thermostat.powerSavingOn();
+        $('#power-saving-status').text('ON')
+        updateTemperature();
+    });
 
-$('#psm-off').click(function() {
-  thermostat.powerSavingOff();
-  $('#power-saving-status').text('OFF')
-  updateTemperature();
-});
+    $('#psm-off').click(function() {
+        thermostat.powerSavingOff();
+        $('#power-saving-status').text('OFF')
+        updateTemperature();
+    });
 
-$('#reset').click(function() {
-  thermostat.reset();
-  updateTemperature();
-});
+    $('#reset').click(function() {
+        thermostat.reset();
+        updateTemperature();
+    });
 
 })
